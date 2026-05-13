ab76d076fec3ddd87a8af4bbf6ec81ac28c5b765
Noor Fatima <nnoorfatima0012@gmail.com>
2026-05-13 08:33:29 +0500

    Fix Client API reference error

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,13 +1,4 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api",
-//   withCredentials: true,
-// });
-
-// export default API;
-
-client/src/api/api.js
+// client/src/api/api.js
 import axios from "axios";
 
 const API = axios.create({
